194f7929cd26ead8301e0af765d5782afd6717c8
PhoonThetPine <thetpine254@gmail.com>
2023-12-04 19:43:56 +0800

    Initial Commit

---
+++ SweatherShopDemo/SweatherShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweatherShopDemoApp.swift
+//  SweatherShopDemo
+//
+//  Created by Phoon Thet Pine on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SweatherShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
